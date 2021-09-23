a43800d2c398e0beff99469e90ce3eafbc9dd7ea
Atharva Joshi <iamatharvaj@gmail.com>
2021-09-23 11:57:45 +0530

    commiting the base project

---
+++ src/components/TransactionListComponent.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import TransactionComponent from "./TransactionComponent";
+import { GlobalContext } from "../context/GlobalState";
+
+export default function TransactionListComponent() {
+  const { transactions } = useContext(GlobalContext);
+  return (
+    <React.Fragment>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <TransactionComponent
+            key={transaction.id}
+            transaction={transaction}
+          />
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+}
