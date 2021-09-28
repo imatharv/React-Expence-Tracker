96d9865ad594a306c33014f73e0674824862ae1a
Atharva Joshi <iamatharvaj@gmail.com>
2021-09-28 19:34:55 +0530

    Refactored user interface

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeaderComponent() {
   return (
     <div className="app-header">
-      <h1 className="app-title">Expence Tracker</h1>
+      <h1 className="app-title">Expense Tracker</h1>
     </div>
   );
 }
